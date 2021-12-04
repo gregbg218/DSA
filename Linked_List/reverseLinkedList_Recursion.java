25db8e80066fc77bdb93081ba82150b8407ee866
gregbg <72642906+gregbg218@users.noreply.github.com>
2021-12-04 13:30:23 +0530

    Update reverseLinkedList_Recursion.java

--- Linked_List/reverseLinkedList_Recursion.java
+++ Linked_List/reverseLinkedList_Recursion.java
@@ -6,6 +6,8 @@ class Solution {
         ListNode newHead = reverseList(head.next);
         ListNode prev = head.next;
         prev.next = head;
+        
+        //This is for when the recursion finally reaches the old head node
         head.next = null;
         return newHead;
     }
