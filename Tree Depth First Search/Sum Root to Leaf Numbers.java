39765c8cc17fbfa72290fe651292bf1f39555a72
gregbg <72642906+gregbg218@users.noreply.github.com>
2023-06-04 12:54:59 +0530

    Create Sum Root to Leaf Numbers.java

---
+++ Tree Depth First Search/Sum Root to Leaf Numbers.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        int resultSum=0;
+        
+        resultSum=sumByDFS(root,0);
+
+        return resultSum;
+    }
+
+    public int sumByDFS(TreeNode root, int pathSum)
+    {
+       
+        if(root==null)    // This condition is for empty trees
+            return 0;
+
+        pathSum=pathSum*10+root.val;
+
+        if(root.left==null && root.right==null) // This is for leaf nodes and the recursion will stop at leaf nodes
+        {
+            
+            return pathSum;     // This is the sum of the paths
+                         
+        } 
+           
+        
+        return sumByDFS(root.left,pathSum)+sumByDFS(root.right,pathSum); 
+
+    }
+}
