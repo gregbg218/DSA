fb43fe8bb4f2ceb74a049813752681bc51c3850b
gregbg <72642906+gregbg218@users.noreply.github.com>
2023-06-17 20:30:32 +0530

    Update Path Sum.java

--- Tree Depth First Search/Path Sum.java
+++ Tree Depth First Search/Path Sum.java
@@ -15,26 +15,25 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        boolean result = checkByDFS(root,targetSum,0);
+        boolean result = checkByDFS(root,targetSum);
 
         return result;
         
     }
 
 
-    public boolean checkByDFS(TreeNode root, int targetSum, int actualSum)
+    public boolean checkByDFS(TreeNode root, int targetSum)
     {
         boolean result ;
         if(root==null)
             return false;
-        actualSum=actualSum+root.val;
-        if(actualSum==targetSum && root.left==null && root.right==null)  // note the sum should be of a root to leaf path and not like if in a tree just the value of the root node is equal to the target
+        if(targetSum==root.val && root.left==null && root.right==null)  // note the sum should be of a root to leaf path and not like if in a tree just the value of the root node is equal to the target
             return true;
         else
         {
-            result = checkByDFS(root.left,targetSum,actualSum);
-            if(result!=true)
-                result = checkByDFS(root.right,targetSum,actualSum);
+            result = checkByDFS(root.left,targetSum-root.val);
+            if(result!=true) // The question only wants 1 path so once we get true we dont need to check further
+                result = checkByDFS(root.right,targetSum-root.val);
         }
         return result;
     }
