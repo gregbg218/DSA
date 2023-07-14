7a66eb5849b7f6ed1d7157e3fe4cb6f76809903e
gregbg <72642906+gregbg218@users.noreply.github.com>
2023-07-14 20:57:04 +0530

    Update Path Sum II.java

--- Tree Depth First Search/Path Sum II.java
+++ Tree Depth First Search/Path Sum II.java
@@ -32,9 +32,7 @@ class Solution {
         if(root.left==null && root.right==null && root.val==targetSum)// we are subtracting from target sum
         {
             allPaths.add(path);
-            return;
-         
-                
+            return;                
         } 
            
         else
