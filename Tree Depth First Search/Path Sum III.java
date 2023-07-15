efce0e22acd80be803a547868ca2dc9bf4f0f68d
gregbg <72642906+gregbg218@users.noreply.github.com>
2023-07-15 19:53:01 +0530

    Update Path Sum III.java

--- Tree Depth First Search/Path Sum III.java
+++ Tree Depth First Search/Path Sum III.java
@@ -1,6 +1,8 @@
 class Solution {
     public int pathSum(TreeNode root, int targetSum) {
         HashMap<Long, Integer> cumulativePathSum = new HashMap<>(); //"long" type becos the currSum usually goes beyond the range of int
+                                                                    // 1st value is cumulative sum and 2nd value is count of that cum. sum
+        
         cumulativePathSum.put(0L, 1);
 
         return countPaths(root, targetSum, 0L, cumulativePathSum);
@@ -10,9 +12,11 @@ class Solution {
         if (node == null)
             return 0;
 
-        currSum += node.val;// getOrDefault just checks if the difference is found in the cumulative sums or not and if not it returns 0
-        int count = cumulativePathSum.getOrDefault(currSum - targetSum, 0); //so the count is just the number of times the target sum has appeared in
-        // the tree. Also count variable is different for every level of recursion so it keeps on changing.
+        currSum += node.val;        // currSum is just the cum. sum(cumulative sum) at the current node
+        
+        int count = cumulativePathSum.getOrDefault(currSum - targetSum, 0);                                                                  
+        //so the count is just the number of times the difference between cum. sum and target sum has appeared in
+        // the tree or not and if not it returns 0. Also count variable is different for every level of recursion so it keeps on changing.
 
         cumulativePathSum.put(currSum, cumulativePathSum.getOrDefault(currSum, 0) + 1); // This is for keeping track of the cumulative sums
 
@@ -20,7 +24,8 @@ class Solution {
         count += countPaths(node.right, targetSum, currSum, cumulativePathSum);
 
         cumulativePathSum.put(currSum, cumulativePathSum.get(currSum) - 1); // once we reach the leaf nodes of a path and dont find the target sum 
-// we remove the associated cumulative sums as it can no longer help us while we explore other paths
+// we remove the associated cumulative sums of each node while backtracking as that particular branch of the tree is done and it can no longer help us 
+//while we explore other paths
 // when we reach the leaf nodes there too we remove the cumulative sums that dont match
         if (cumulativePathSum.get(currSum) == 0)//it basically saves memory by removing cumulative sums with 0 frequencies and 
             cumulativePathSum.remove(currSum);//doesnt affect accuracy of soln. The code still works if we comment it
