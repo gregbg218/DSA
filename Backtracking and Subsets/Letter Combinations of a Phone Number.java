8461470f33b5780f07c3b488317ef953531402dd
gregbg <72642906+gregbg218@users.noreply.github.com>
2023-07-19 13:25:37 +0530

    Create Letter Combinations of a Phone Number.java

---
+++ Backtracking and Subsets/Letter Combinations of a Phone Number.java
@@ -0,0 +1,42 @@
+class Solution {
+    List<String> result = new ArrayList<>();
+  
+    HashMap<Character, String> numberToCharacters = new HashMap<>();
+
+    public List<String> letterCombinations(String digits) {
+        if (digits.length() == 0)
+            return result;
+
+        initializeNumberToCharacters();
+
+        backTrackAndFind(0, "", digits);
+
+        return result;
+    }
+
+    public void initializeNumberToCharacters() {
+        numberToCharacters.put('2', "abc");
+        numberToCharacters.put('3', "def");
+        numberToCharacters.put('4', "ghi");
+        numberToCharacters.put('5', "jkl");
+        numberToCharacters.put('6', "mno");
+        numberToCharacters.put('7', "pqrs");
+        numberToCharacters.put('8', "tuv");
+        numberToCharacters.put('9', "wxyz");
+    }
+
+    public void backTrackAndFind(int digitPosition, String currStr, String inputDigits) {
+        if (currStr.length() == inputDigits.length()) {
+            result.add(currStr);
+            return;
+        }
+
+        char digit = inputDigits.charAt(digitPosition);
+        String charactersAtDigit = numberToCharacters.get(digit);
+
+        for (char c : charactersAtDigit.toCharArray()) {
+            backTrackAndFind(digitPosition + 1, currStr + c, inputDigits);// String datatype is immutable thats why we are not doing currStr=currStr + c
+                                                                          // as that makes us lose the old value of string which we need after backtracking
+        }
+    }
+}
