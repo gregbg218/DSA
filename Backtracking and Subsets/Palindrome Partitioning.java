c334ecdbd27ce685bf629f164064c84dd33ab03a
gregbg <72642906+gregbg218@users.noreply.github.com>
2023-09-21 21:14:57 +0530

    Create Palindrome Partitioning.java

---
+++ Backtracking and Subsets/Palindrome Partitioning.java
@@ -0,0 +1,72 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<String> tempStr= new ArrayList<String>();
+        List<List<String>> res= new ArrayList<>();
+        backTrackPalindrome(0,tempStr,res,s);
+        return res;
+    }
+
+    public void backTrackPalindrome(int startPos,List<String> tempStr,List<List<String>> res,String givenString)
+    {
+        if(startPos==givenString.length())
+            res.add(new ArrayList<>(tempStr));
+        else
+        {
+            for(int i=startPos;i<givenString.length();i++)
+            {
+                printString(tempStr);
+                System.out.println("Pal is from ("+startPos+","+i+")");
+                if(checkPalindrome(startPos,i,givenString))
+                {
+                    System.out.print("Adding ("+startPos+","+(i+1)+") ");
+                    System.out.println(givenString.substring(startPos,i+1));
+                    System.out.println();
+                    System.out.println();
+                    tempStr.add(givenString.substring(startPos,i+1));
+                    backTrackPalindrome(i+1,tempStr,res,givenString);
+                    tempStr.remove(tempStr.size()-1);
+                }
+                else
+                {
+                    System.out.println("Not Pal");
+                    System.out.println();
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+    public boolean checkPalindrome(int low, int high,String givenString)
+    {
+        while(low<high)
+        {
+            if(givenString.charAt(low++)!=givenString.charAt(high--))  // first comparison is done and then in the next iteration there is increment 
+                                                                       // and decrement
+                return false;
+        }
+        return true;
+    }
+  
+    public void printString(List<String> tempStr)
+    {
+
+        StringBuilder result = new StringBuilder();
+        for (String element : tempStr) {
+        if (result.length() > 0) {
+            result.append(","); // Add a comma and space except for the first element
+        }
+        result.append(element);
+        }
+
+        // Print the concatenated string
+        System.out.println(result.toString());
+
+    }
+}
+
+
+// Notice checkPalindrome is from (3,3) and Adding is from (3,4). Both are same but becos the subString method requires 1 more 
+
+
+// What is basically happening is that at each iteration u are adding one letter at a time by checking if the letter at i+1 th index is a
+// palindrome or not
