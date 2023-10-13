cab694ad6ac4d3a3d349a3962c4c071710c5d87c
gregbg <72642906+gregbg218@users.noreply.github.com>
2023-10-13 12:34:34 +0530

    Update Floor and ceil in sorted array.java

--- modified binary search/Floor and ceil in sorted array.java
+++ modified binary search/Floor and ceil in sorted array.java
@@ -73,3 +73,25 @@ class Solution {
 
     }
 }
+// In the context of an array of numbers, the "floor" and "ceiling" of a given value refer to 
+//finding the largest number in the array that is less than or equal to the given value (floor) and the smallest
+//number in the array that is greater than or equal to the given value (ceiling).
+
+// For your example array {1, 2, 8, 10, 10, 12, 19}, let's calculate the floor and ceiling for the specified values:
+
+// For x = 0:
+// Floor: There is no number in the array that is less than or equal to 0, so the floor doesn't exist.
+// Ceiling: The smallest number in the array that is greater than or equal to 0 is 1, so the ceiling is 1.
+
+// For x = 1:
+// Floor: The largest number in the array that is less than or equal to 1 is 1 itself, so the floor is 1.
+// Ceiling: The smallest number in the array that is greater than or equal to 1 is also 1, so the ceiling is 1.
+
+// For x = 5:
+// Floor: The largest number in the array that is less than or equal to 5 is 2, so the floor is 2.
+// Ceiling: The smallest number in the array that is greater than or equal to 5 is 8, so the ceiling is 8.
+
+// For x = 20:
+// Floor: The largest number in the array that is less than or equal to 20 is 19, so the floor is 19.
+// Ceiling: There is no number in the array that is greater than or equal to 20, so the ceiling doesn't exist in the array.
+
