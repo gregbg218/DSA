32498edc84aac483db875e2c46321a108352321d
gregbg <72642906+gregbg218@users.noreply.github.com>
2023-10-25 09:48:00 +0530

    Update Kth Smallest Element.java

--- Top K elements/Kth Smallest Element.java
+++ Top K elements/Kth Smallest Element.java
@@ -1,32 +1,16 @@
-import java.util.Collections;
-import java.util.PriorityQueue;
-
 class Solution {
-    public static int findKSmallestElement(int[] arr, int k) {
-        PriorityQueue<Integer> maxHeap= new PriorityQueue<>(Collections.reverseOrder());
+    public int findKthLargest(int[] arr, int k) {
+        PriorityQueue<Integer> minHeap= new PriorityQueue<>();
 
         for(int i =0;i<arr.length;i++)
         {
-            maxHeap.add(arr[i]);
-            if(maxHeap.size()>k)
+            minHeap.add(arr[i]);
+            if(minHeap.size()>k)
             {
-                maxHeap.poll();
+                minHeap.poll();
             }
         }
-        return maxHeap.peek();
-
-    }
-
-
-    public static void main(String[] args)
-    {
-
-        int[] arr = {7, 10, 4, 3, 20, 15};
-        int k = 3;
-
-        System.out.println("K smallest Element is : "+ findKSmallestElement(arr,k));
-        // 3,4,7,10,15,20
+        return minHeap.peek();
+        
     }
 }
-
-
