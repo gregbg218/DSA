989bebb10f9149fff8481dbf3fa6a4e8a07caf13
gregbg <72642906+gregbg218@users.noreply.github.com>
2023-10-27 10:04:33 +0530

    Create Top K Frequent Elements.java

---
+++ Top K elements/Top K Frequent Elements.java
@@ -0,0 +1,45 @@
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int[] topKFrequent(int[] arr, int k) {
+        int[] res = new int[k];
+        Map<Integer,Integer> freqMap = new HashMap<Integer,Integer>();
+
+        for(int i=0;i<arr.length;i++)
+        {
+            freqMap.put(arr[i], freqMap.getOrDefault(arr[i],0)+1);
+        }
+
+        Comparator<Integer> freqComparator = new Comparator() 
+        {
+            @Override
+            public int compare(Object o, Object t1) {
+                return freqMap.get(o)-freqMap.get(t1);
+            }
+            
+        };
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(freqComparator);
+
+        for(int el : freqMap.keySet())
+        {
+            minHeap.add(el);
+            if(minHeap.size()>k)
+            {
+                minHeap.poll();
+            }
+        }
+
+        k=k-1;
+        while(minHeap.size()!=0)
+        {
+            res[k--]=minHeap.poll();
+
+        }
+
+
+        return res;
+    }
+}
