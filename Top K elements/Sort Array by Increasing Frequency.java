78824571b291435a515b63b0d204d45d969851bb
gregbg <72642906+gregbg218@users.noreply.github.com>
2023-10-28 10:31:32 +0530

    Create Sort Array by Increasing Frequency.java

---
+++ Top K elements/Sort Array by Increasing Frequency.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int[] frequencySort(int[] arr) {
+        Map<Integer,Integer> freqMap = new HashMap<>();
+
+        for(int i=0;i<arr.length;i++)
+        {
+            freqMap.put(arr[i], freqMap.getOrDefault(arr[i],0)+1);
+        }
+        // Above step finds the frequency of all elements
+
+        Comparator<Integer> freqComparator = new Comparator()  // So this comparator just uses the particular hashpMp to compare things in that heap
+        {
+            @Override
+            public int compare(Object o, Object t1) {
+                Integer a=(int)o;
+                Integer b=(int)t1;
+                Integer aFreq=freqMap.get(a);
+                Integer bFreq=freqMap.get(b);
+
+                if(Objects.equals(aFreq, bFreq))
+                    return a.compareTo(b);      // Ascending order
+                return bFreq.compareTo(aFreq);  // Descending order
+            }
+
+        };
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(freqComparator);
+
+        for(int el : freqMap.keySet())
+        {
+            maxHeap.add(el);
+
+        }
+
+        int k=arr.length-1;            // just to act as a counter for the array below
+        while(maxHeap.size()!=0)
+        {
+            int element = maxHeap.poll();
+            int freq = freqMap.get(element);
+            while(freq!=0)
+            {
+                arr[k--] = element;
+                freq--;
+            }
+
+        }
+
+
+        return arr;
+        
+    }
+}
