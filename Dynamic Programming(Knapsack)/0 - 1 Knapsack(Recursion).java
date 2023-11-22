4f8079783f2caf54727743faa45927d42bbc478c
gregbg <72642906+gregbg218@users.noreply.github.com>
2023-11-22 08:27:50 +0530

    Update 0 - 1 Knapsack(Recursion).java

--- Dynamic Programming(Knapsack)/0 - 1 Knapsack(Recursion).java
+++ Dynamic Programming(Knapsack)/0 - 1 Knapsack(Recursion).java
@@ -5,11 +5,11 @@ class Solution
     { 
        if(n==0 || W==0)
             return 0;
-        if(wt[n-1]<=W)
-        {
-            return Math.max(val[n-1]+knapSack(W-wt[n-1],wt, val,n-1),knapSack(W,wt, val,n-1));    
-        }
-        else
-            return knapSack(W,wt, val,n-1);
+       if(wt[n-1]<=W)
+       {
+        return Math.max(val[n-1]+knapSack(W-wt[n-1],wt, val,n-1),knapSack(W,wt, val,n-1));    
+       }
+       else
+        return knapSack(W,wt, val,n-1);
     } 
 }
