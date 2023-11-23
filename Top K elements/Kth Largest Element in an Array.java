e5c87cfe2d70a46c956e2172be04e14a98f396e6
gregbg <72642906+gregbg218@users.noreply.github.com>
2023-11-23 21:44:05 +0530

    Update Kth Largest Element in an Array.java

--- Top K elements/Kth Largest Element in an Array.java
+++ Top K elements/Kth Largest Element in an Array.java
@@ -19,3 +19,6 @@ class Solution {
 
 // By the time it reaches the final element of array there are k+1 or 3 of the largst element on the heap then we pop the 3rd largest and break out 
 // of loop. Then at the top of heap we have kth or 2nd largest element
+
+
+// so our aim is to get the k largest elements in the heap after traversing the whole array and also keeping time complexity low
