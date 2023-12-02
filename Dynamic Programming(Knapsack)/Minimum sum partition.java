58f3ba06b63520c1992753fbb2ce1b70d7f33799
gregbg <72642906+gregbg218@users.noreply.github.com>
2023-12-02 10:18:25 +0530

    Create Minimum sum partition.java

---
+++ Dynamic Programming(Knapsack)/Minimum sum partition.java
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// There are going to be 2 subsets s1 and s2
+// our goal is that s1-s2 should be minimum
+// s1 can be written as s1=Range-s2
+// Substituting in s1-s2=(Range-s2)-s2=Range-2(s2)
+// So our goal is to find some value of s2 that can minimize Range-2(s2)
+// Only possible way is that s2<=(Range)/2
+// becos if s2==(Range)/2 then the difference is 0 which is also acceptable
+// but s2 shouldnt be more than (Range)/2
