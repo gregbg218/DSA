bcc61587fe9ed293b2d92270e2df8d04d357ea33
gregbg <72642906+gregbg218@users.noreply.github.com>
2023-12-04 11:36:07 +0530

    Update Equal Sum Partition.java

--- Dynamic Programming(Knapsack)/Equal Sum Partition.java
+++ Dynamic Programming(Knapsack)/Equal Sum Partition.java
@@ -19,21 +19,18 @@ class Solution {
 
     public boolean isSubsetSum(int n, int set[], int sum) 
     {
-    boolean[][] dpTable = new boolean[n+1][sum+1];
+        boolean[][] dpTable = new boolean[n+1][sum+1];
     
-    for(int i=0;i<=n;i++)
-       {
-           for(int j=0;j<=sum;j++)
-           {
-               if(i==0)
-                dpTable[i][j]=false;
-                
-               if(j==0)
-                dpTable[i][j]=true;
-              
-           }
+    
+        for(int j=0;j<=sum;j++)
+        {
+               
+            dpTable[0][j]=false;         
+        }
            
-       }
+        
+
+	   dpTable[0][0]=true;
        
        for(int i=1;i<=n;i++)
        {
