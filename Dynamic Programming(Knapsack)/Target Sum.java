7fdfd2c38bc4dead1647f6200137b9f8d8d87c1e
gregbg <72642906+gregbg218@users.noreply.github.com>
2023-12-04 11:40:43 +0530

    Update Target Sum.java

--- Dynamic Programming(Knapsack)/Target Sum.java
+++ Dynamic Programming(Knapsack)/Target Sum.java
@@ -16,17 +16,15 @@ class Solution {
         int n=arr.length;
 
         int[][] t= new int[n+1][sum+1];
+        
        for(int i=0;i<sum+1;i++)
             t[0][i] = 0;
        
-       for(int i=0;i<n+1;i++)
-            t[i][0] = 1;
-        
-
+  
+        t[0][0] = 1;
          
         for(int i=1;i<n+1;i++)
-            for(int j=0;j<sum+1;j++){   // Usually j starts from 1 but for a special edge case we are starting from 0 ignore the reason
-                                        // Always start from i=1 and j=1 exception is made only for this sum
+            for(int j=0;j<sum+1;j++){
                 if(arr[i-1] <= j)
                     t[i][j] = (t[i-1][j] + t[i-1][j-arr[i-1]]);
                 else
