f38dea8dccffe9a99671d4eb67c57c44b8af53fb
gregbg <72642906+gregbg218@users.noreply.github.com>
2023-12-04 11:32:21 +0530

    Update Subset Sum.java

--- Dynamic Programming(Knapsack)/Subset Sum.java
+++ Dynamic Programming(Knapsack)/Subset Sum.java
@@ -4,19 +4,15 @@ class Solution{
    static boolean isSubsetSum(int n, int set[], int sum) {
     boolean[][] dpTable = new boolean[n+1][sum+1];
     
-    for(int i=0;i<=n;i++)
-       {
-           for(int j=0;j<=sum;j++)
+    for(int j=0;j<=sum;j++)
            {
-               if(i==0)
-                dpTable[i][j]=false;
-                
-               if(i==0 && j==0)
-                dpTable[i][j]=true;
-              
+               
+                dpTable[0][j]=false;         
            }
            
-       }
+        
+
+	   dpTable[0][0]=true;
        
        for(int i=1;i<=n;i++)
        {
