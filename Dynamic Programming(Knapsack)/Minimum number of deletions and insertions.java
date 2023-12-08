5042ae51592ba9c21e5b5d5aff98252f5fcd7e3d
gregbg <72642906+gregbg218@users.noreply.github.com>
2023-12-08 23:22:26 +0530

    Update Minimum number of deletions and insertions.java

--- Dynamic Programming(Knapsack)/Minimum number of deletions and insertions.java
+++ Dynamic Programming(Knapsack)/Minimum number of deletions and insertions.java
@@ -1,26 +1,4 @@
-//{ Driver Code Starts
-//Initial Template for Java
-import java.io.*;
-import java.util.*;
-class GfG
-{
-    public static void main(String args[])
-        {
-            Scanner sc = new Scanner(System.in);
-            int t = sc.nextInt();
-            while(t-->0)
-                {
-                    String s1 = sc.next();
-                    String s2 = sc.next();
-                    Solution ob = new Solution();
-                    System.out.println(ob.minOperations(s1,s2));
-                }
-        }
-}    
-// } Driver Code Ends
 
-
-//User function Template for Java
 
 class Solution
 {
