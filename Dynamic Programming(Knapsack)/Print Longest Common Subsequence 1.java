16e53e7c0525a8e8dc9774efb8039ed79d78744a
gregbg <72642906+gregbg218@users.noreply.github.com>
2023-12-08 11:26:53 +0530

    Create Print Longest Common Subsequence 1.java

---
+++ Dynamic Programming(Knapsack)/Print Longest Common Subsequence 1.java
@@ -0,0 +1,49 @@
+public class Solution {
+    public static String findLCS(int n, int m, String str1, String str2){
+        // int n=str1.length();
+        // int m=str2.length();
+        int dp[][]=new int[n+1][m+1];
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=m;j++)
+            {
+                if(i==0 || j==0)
+                {
+                    dp[i][j]=0;
+                }
+                else if(str1.charAt(i-1)==str2.charAt(j-1))
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        StringBuffer bf = new StringBuffer("");
+        int i=n;
+        int j=m;
+        while(i>0 && j>0)
+        {
+            if(str1.charAt(i-1)==str2.charAt(j-1))
+            {
+                bf.append(str1.charAt(i-1));
+                i--;
+                j--;
+            }
+            else if(dp[i-1][j]>dp[i][j-1])
+            {
+                // bf.append(str1.charAt(i-1));
+                i--;
+            }
+            else
+            {
+                // bf.append(str2.charAt(j-1));
+                j--;
+            }
+        }
+
+        return bf.reverse().toString();
+    }
+}
