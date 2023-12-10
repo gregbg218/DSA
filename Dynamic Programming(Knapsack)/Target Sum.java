8683f900c9f96eb2cc74882f0719aeee6e2302e3
gregbg <72642906+gregbg218@users.noreply.github.com>
2023-12-11 00:07:22 +0530

    Update Target Sum.java

--- Dynamic Programming(Knapsack)/Target Sum.java
+++ Dynamic Programming(Knapsack)/Target Sum.java
@@ -39,3 +39,11 @@ class Solution {
 //Why do we do t[0][0]=1;
 //Becos When the target sum is 0 and you have an empty set (a={}), there is one way 
 //to achieve this sum, and that is by having an empty subset.
+
+// see in the video from 3:40 how it
+//uses same logic as count of subset sum difference
+// s1+s2 = total sum
+// s1-s2= difference=given target sum in the question
+// so Adding above equations we get 2s1=total sum+difference
+//s1=(total sum+difference)/2
+//substitute the values
