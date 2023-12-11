3866e9d35a40efe40fb87a11aa9051177655f37e
gregbg <72642906+gregbg218@users.noreply.github.com>
2023-12-11 13:40:59 +0530

    Create Minimum number of insertion in a string to make it a palindrome.java

---
+++ Dynamic Programming(Knapsack)/Minimum number of insertion in a string to make it a palindrome.java
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+// Here we are using 2 past questions Minimum number of Deletions to make palindrome and Longest Palindromic SubSequence
+// Basically if string = abffea then to make palindrome using deletions we get palindromic subsequence as affa and we need to make 2 deletions
+// b and e
+
+// Now the thing is this is same as saying if we insert an "e" after "b" and a "b" after the 2nd last "e" then it becomes a palindrome
+// So basically number of deletions= no of insertions
+
+
+
+
+
+
