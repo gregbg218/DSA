8f628ee7ec54dcc87016dd6c1064209fd5d3b4c0
gregbg <72642906+gregbg218@users.noreply.github.com>
2024-01-17 19:03:33 -0800

    Update Count of subset sum.java

--- Dynamic Programming(Knapsack)/Count of subset sum.java
+++ Dynamic Programming(Knapsack)/Count of subset sum.java
@@ -30,6 +30,9 @@ class Solution
 };
 
 
+// instead of doing Max((t[i-1][j] + t[i-1][j-arr[i-1]])) we are just doing (t[i-1][j] + t[i-1][j-arr[i-1]]); as this just accepts all possibile counts instead of finding
+// max or min and limiting possibile sums
+
 //Why do we do dpTable[0][0]=1;
 //Becos When the target sum is 0 and you have an empty set (a={}), there is one way 
 //to achieve this sum, and that is by having an empty subset.
