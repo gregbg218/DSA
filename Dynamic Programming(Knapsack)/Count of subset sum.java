633552e317837ec9ff9c3c6bd9801bb4fd0c3672
gregbg <72642906+gregbg218@users.noreply.github.com>
2024-02-24 20:17:46 -0800

    Update Count of subset sum.java

--- Dynamic Programming(Knapsack)/Count of subset sum.java
+++ Dynamic Programming(Knapsack)/Count of subset sum.java
@@ -38,10 +38,16 @@ class Solution
 //to achieve this sum, and that is by having an empty subset.
 
 // this question is same as subset sum only difference is we want the number of possible ways instead of yes or no
-// so here too at each step we are saying there are 2 ways:
+
+// in this line :t[i][j] = (t[i-1][j] + t[i-1][j-arr[i-1]]);
+// at each step we are saying there are 2 ways:
 //including (i-1)th index element or not including (i-1)th index element
 //example: 5,11 is obtained from 4,1 OR 4,11
 //(i-1)th index element in the above example is 10
 // when including 10 then we can subtract and if there's a remainder use the previous subproblem for getting the remainder sum/weight
 // when not including 10 then we are forced to use the previous subproblem for getting the full sum/weight 
+// the unsuccessful way would give value 0
 
+
+// however note in this type of sums((t[i-1][j] + t[i-1][j-arr[i-1]])) where the real work is done by j-arr[i-1] there should be
+// a base case where the value is 1 like here t[0][0] = 1;
