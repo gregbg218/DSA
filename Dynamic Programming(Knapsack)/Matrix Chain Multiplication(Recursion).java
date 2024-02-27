4a86496f6c30f2556173a96af8a4095e0a262595
gregbg <72642906+gregbg218@users.noreply.github.com>
2024-02-26 20:57:57 -0800

    Update Matrix Chain Multiplication(Recursion).java

--- Dynamic Programming(Knapsack)/Matrix Chain Multiplication(Recursion).java
+++ Dynamic Programming(Knapsack)/Matrix Chain Multiplication(Recursion).java
@@ -1,31 +1,18 @@
 class Solution{
     static int matrixMultiplication(int N, int arr[])
     {
-        int result=Integer.MAX_VALUE;
-        int[][] memoTable = new int[N+1][N+1]; 
-        for(int i=0;i<=N;i++)
-       {
-           for(int j=0;j<=N;j++)
-           {
-               memoTable[i][j]=-1;
-           }
-       }
-        return solveByDP(1,N-1, arr,result,memoTable);
+        return solveByDP(1,N-1, arr);
         
     }
     
-    static int solveByDP(int i, int j, int arr[],int result,int[][] memoTable)
+    static int solveByDP(int i, int j, int arr[])
     {
         
         
         if(i>=j)
             return 0;
             
-        if (memoTable[i][j] != -1)  
-        { 
-            return memoTable[i][j]; 
-        }
-        
+
         int cost=Integer.MAX_VALUE;
         
         for(int k=i;k<=j-1;k++)
@@ -33,14 +20,12 @@ class Solution{
             cost = 
             Math.min(
             cost,
-            solveByDP(i,k,arr,result,memoTable)+solveByDP(k+1,j,arr,result,memoTable)+arr[i-1]*arr[k]*arr[j]
+            solveByDP(i,k,arr)+solveByDP(k+1,j,arr)+arr[i-1]*arr[k]*arr[j]
             );
             
         }
-            
-        memoTable[i][j] = cost;
         
-        //For every i,j we find the way of minimum cost using k and store it in memo table
+        //For every i,j(i and j is a way of dividng the array) we find the way of minimum cost using k 
         return cost;
     }
 }
