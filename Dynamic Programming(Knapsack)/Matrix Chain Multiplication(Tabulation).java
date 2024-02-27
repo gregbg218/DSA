c46001daac06c322c4e759e0669175265f7e947f
gregbg <72642906+gregbg218@users.noreply.github.com>
2024-02-27 10:06:24 -0800

    Update Matrix Chain Multiplication(Tabulation).java

--- Dynamic Programming(Knapsack)/Matrix Chain Multiplication(Tabulation).java
+++ Dynamic Programming(Knapsack)/Matrix Chain Multiplication(Tabulation).java
@@ -9,13 +9,13 @@ class Solution{
     static int matrixMultiplication(int N, int arr[])
     {
        
-        int[][] memoTable = new int[N][N]; 
+        int[][] dpTable = new int[N][N]; 
         
-        return solveByDP(arr,memoTable);
+        return solveByDP(arr,dpTable);
         
     }
     
-    static int solveByDP(int arr[],int[][] memoTable)
+    static int solveByDP(int arr[],int[][] dpTable)
     {
         int N=arr.length;
         for(int i=0;i<N;i++)
@@ -23,7 +23,7 @@ class Solution{
             for(int j=0;j<N;j++)
             {
                 if(i>=j || i==0)     // i==0 becos i always starts from 1 and i>=j is the base case of memoization
-                    memoTable[i][j] = 0;
+                    dpTable[i][j] = 0;
             
             }
         }
@@ -36,14 +36,14 @@ class Solution{
                     for(int k=i;k<=j-1;k++)
                     {
                         cost=Math.min(
-                            memoTable[i][k]+memoTable[k+1][j]+arr[i-1]*arr[k]*arr[j],
+                            dpTable[i][k]+dpTable[k+1][j]+arr[i-1]*arr[k]*arr[j],
                             cost
                             
                             );
                         ;
                     }
 
-                    memoTable[i][j]=cost;
+                    dpTable[i][j]=cost;
                 
                 
             }
@@ -52,7 +52,7 @@ class Solution{
 
         
         
-        return memoTable[1][N-1];
+        return dpTable[1][N-1];
         
         
     }
