d840fca6a25c1bbdce8bb76ecceb7f11e204761b
gregbg <72642906+gregbg218@users.noreply.github.com>
2024-03-28 15:59:46 -0700

    Update Frog Jump(Recursion).java

--- Dynamic Programming(Knapsack)/Frog Jump(Recursion).java
+++ Dynamic Programming(Knapsack)/Frog Jump(Recursion).java
@@ -6,7 +6,7 @@ public class Solution {
         if(n==1)
             return 0;
 
-        int jump1 =  Math.abs(heights[n-1]-heights[n-2])+frogJump(n-1, heights);
+        int jump1 =  Math.abs(heights[n-1]-heights[n-2])+frogJump(n-1, heights);  // term 1 is jump from 1 to 2 and term 2 is function for starting from 2
 
         int jump2 = Integer.MAX_VALUE;
         if(n-2>0)
